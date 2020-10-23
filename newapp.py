904d7e7d25bc90118f95882301b19f920cd8cb91
JAYESH DILIP RAUNDAL <jdraundal@yahoo.com>
2020-10-23 18:41:12 +0530

    new app added

---
+++ newapp.py
@@ -0,0 +1,40 @@
+from flask import Flask,render_template,request
+from mysql import connector
+ 
+app=Flask(__name__)
+
+
+
+
+
+mydb = connector.connect(
+    host="localhost",
+    user="root",
+    passwd="9960551687",
+    database="bankdb")
+
+mycur = mydb.cursor()
+
+        
+
+@app.route('/',methods=['GET','POST'])
+def index():
+	if request.method=='POST':
+
+	
+	 
+		name=request.form['name']
+		email=request.form['email']
+		 
+		query="insert into users(name,email) values(%s,%s)"
+		val=(name,email)
+		mycur.execute(query,val)	
+		mydb.commit()
+		print('data inserted sucessfuly')
+		
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
+		
+
